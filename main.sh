d13eade091abd90707298474ee43acedaa37b7a9
unknownjustuser <unknown.just.user@proton.me>
2024-03-27 12:19:11 +0530

    bruh

--- main.sh
+++ main.sh
@@ -14,7 +14,7 @@ SCRIPTS_DIR="scripts"
 # Check if a command is provided
 if [[ $# -eq 0 ]]; then
   echo "Usage: $0 <command>"
-  echo "Available commands: setup, gensum, build-txt, build-pkgbuild, push, check, srcinfo"
+  echo "Available commands: setup, gensum, build-txt, build-pkgbuild, push, srcinfo"
   exit 1
 fi
 
@@ -23,34 +23,30 @@ for cmd in "$@"; do
   case "$cmd" in
   setup)
     chmod +x "$SCRIPTS_DIR"/setup.sh
-    ./"$SCRIPTS_DIR/setup.sh"
+    ./"$SCRIPTS_DIR/setup.sh" &>/dev/null
     ;;
   gensum)
     chmod +x "$SCRIPTS_DIR"/gensum.sh
-    ./"$SCRIPTS_DIR"/gensum.sh
+    ./"$SCRIPTS_DIR"/gensum.sh &>/dev/null
     ;;
   build-txt)
     chmod +x "$SCRIPTS_DIR"/build-txt.sh
-    ./"$SCRIPTS_DIR/build-txt.sh"
+    ./"$SCRIPTS_DIR/build-txt.sh" &>/dev/null
     ;;
   build-pkgbuild)
     chmod +x "$SCRIPTS_DIR"/build-pkgbuild.sh
-    ./"$SCRIPTS_DIR/build-pkgbuild.sh"
+    ./"$SCRIPTS_DIR/build-pkgbuild.sh" &>/dev/null
     ;;
   push)
     chmod +x "$SCRIPTS_DIR"/push.sh
-    ./"$SCRIPTS_DIR/push.sh"
-    ;;
-  check)
-    chmod +x "$SCRIPTS_DIR"/check.sh
-    ./"$SCRIPTS_DIR/check.sh"
+    ./"$SCRIPTS_DIR/push.sh" &>/dev/null
     ;;
   srcinfo)
     chmod +x "$SCRIPTS_DIR"/srcinfo.sh
-    ./"$SCRIPTS_DIR/srcinfo.sh"
+    ./"$SCRIPTS_DIR/srcinfo.sh" &>/dev/null
     ;;
   *)
-    echo "Invalid command. Available commands: setup, gensum, build-txt, build-pkgbuild, push, check, srcinfo"
+    echo "Invalid command. Available commands: setup, gensum, build-txt, build-pkgbuild, push, srcinfo"
     exit 1
     ;;
   esac
