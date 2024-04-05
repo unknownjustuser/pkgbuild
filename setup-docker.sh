3ae3a6bfd86d791fbebb0f8562ff1af10693e0bc
unknownjustuser <unknown.just.user@proton.me>
2024-04-05 15:05:40 +0530

    This commit adds a bash script for installing packages and updates pacman configurations to include the BlackArch repository.

--- setup-docker.sh
+++ setup-docker.sh
@@ -2,26 +2,6 @@
 
 pacman -Scc --noconfirm
 pacman -Syy
-pacman-key --init
-pacman-key --keyserver hkp://keyserver.ubuntu.com:443 --recv-key 3056513887B78AEB
-pacman-key --lsign-key 3056513887B78AEB
-pacman --noconfirm -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
-echo "[multilib]" >>/etc/pacman.conf && echo "Include = /etc/pacman.d/mirrorlist" >>/etc/pacman.conf
-echo -e "\\n[chaotic-aur]\\nInclude = /etc/pacman.d/chaotic-mirrorlist" >>/etc/pacman.conf
-echo "" >>/etc/pacman.conf
-pacman -Syy --noconfirm --quiet wget
-bash <(wget -qO- https://blackarch.org/strap.sh)
-
-if grep -q "\[multilib\]" /etc/pacman.conf; then \
-  sed -i '/^\[multilib\]/,/Include = \/etc\/pacman.d\/mirrorlist/ s/^#//' /etc/pacman.conf; \
-else \
-  echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | tee -a /etc/pacman.conf; \
-fi
-
-sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo 'KEYMAP=us' > /etc/vconsole.conf
 
 pacman -Syy --noconfirm --quiet --needed archlinux-keyring blackarch-keyring yay archiso audit aurutils cmake curl devtools libinput docker docker-buildx docker-compose glibc-locales gnupg grep gzip jq less make man namcap openssh openssl parallel pkgconf python python-apprise python-pip rsync squashfs-tools tar unzip vim wget yq zip paru reflector git-lfs openssh git namcap audit grep diffutils parallel cronie
 
